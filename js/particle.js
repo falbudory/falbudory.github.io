d631198ec5497b6eec8fd36965b145fb5521b937
DESKTOP-P3UG0UK\HA <falbudory@gmail.com>
2023-06-06 09:29:51 +0700

    no message

--- js/particle.js
+++ js/particle.js
@@ -3,7 +3,7 @@
  */
 var settings = {
   particles: {
-    length: 500, // maximum amount of particles
+    length: 800, // maximum amount of particles
     duration: 2, // particle duration in sec
     velocity: 100, // particle velocity in pixels/sec
     effect: -0.75, // play with this for a nice effect
